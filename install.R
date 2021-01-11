ee84238be89bd470a7c4ac000361c9bf2ada08a4
adrianna112132 <62908292+adrianna112132@users.noreply.github.com>
2021-01-11 08:52:58 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,4 +1,2 @@
 install.packages('tidyverse') ##przetwarzanie
-install.packages('data.table') ## do ramek danych
 install.packages('readxl') ## wczytanie plików excela
-install.packages('sf') ##wczytanie plików z mapami
